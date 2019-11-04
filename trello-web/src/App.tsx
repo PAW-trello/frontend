6c6d719e2d070bfb2aeb793f7f0df5b3c049d485
wsobczyk <wiktoriasobczyk@gmail.com>
2019-11-04 09:10:09 +0100

    test deploy -v2

--- trello-web/src/App.tsx
+++ trello-web/src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test
+          Learn React testt
         </a>
       </header>
     </div>
