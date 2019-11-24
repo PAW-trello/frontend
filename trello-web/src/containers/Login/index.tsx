e308040f9662f8ba42b798368de3729eb0ad6599
wsobczyk <wiktoriasobczyk@gmail.com>
2019-11-24 17:27:59 +0100

    Added mockup forms for login and register

--- trello-web/src/containers/Login/index.tsx
+++ trello-web/src/containers/Login/index.tsx
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from "reactstrap";
 
 export default class Login extends Component {
-    render() {
-        return (
-            <div>
-                <h3>React Login Component</h3>
-            </div>
-        );
-    }
+  render() {
+    return (
+        <Container>
+            <Row>
+        <Col sm="12" md={{ size: 6, offset: 3 }}> <Form>
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="with a placeholder"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              id="examplePassword"
+              placeholder="password placeholder"
+            />
+          </FormGroup>
+
+          <Button>Zaloguj</Button>
+        </Form>
+</Col>
+      </Row>
+   </Container>
+       
+    );
+  }
 }
--- trello-web/src/containers/Register/index.tsx
+++ trello-web/src/containers/Register/index.tsx
@@ -1,11 +1,56 @@
 import React, { Component } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
 
 export default class Register extends Component {
-    render() {
-        return (
-            <div>
-                <h3>React Register Component</h3>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            {" "}
+            <Form>
+              <FormGroup>
+                <Label for="exampleEmail">Nazwa użytkownika</Label>
+                <Input
+                  type="username"
+                  name="username"
+                  id="username"
+                  placeholder="with a placeholder"
+                />
+                        </FormGroup>
+                        <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="with a placeholder"
+            />
+          </FormGroup>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="examplePassword"
+                  placeholder="password placeholder"
+                />
+              </FormGroup>
+
+              <Button>Zarejestruj się</Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
